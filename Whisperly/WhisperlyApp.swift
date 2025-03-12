51ea76b687b3226053ecbf88732e3c8728b0cc7b
Jeroen Jonckheer <jeroen@jonckheer.net>
2025-03-12 14:59:24 +0100

    Initial Commit

---
+++ Whisperly/WhisperlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperlyApp.swift
+//  Whisperly
+//
+//  Created by user273034 on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisperlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
